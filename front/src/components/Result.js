6c64ee21cfaf1918715b31cf02b64fd7a7347cf6
Nysskyo <58655088+Nysskyo@users.noreply.github.com>
2022-10-15 17:46:53 +0900

    無駄な部分を消した

---
+++ front/src/components/Result.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+const Result = (props) => {
+  return (
+    <div>
+      <h2>Result</h2>
+
+      <div>
+        判定結果 : {props.message}
+      </div>
+
+      <div>
+        <img src={`data:image/png;base64,${props.image}`} />
+      </div>
+
+      <div>
+      <button onClick={props.reset}>Reset</button>
+      </div>
+    </div>
+  );
+}
+
+export default Result;
