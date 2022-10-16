b28ac168ec933f06d530048bcaaec9d1aa079b96
Nysskyo <58655088+Nysskyo@users.noreply.github.com>
2022-10-16 20:33:21 +0900

    見た目を少し整えた

--- front/src/App.js
+++ front/src/App.js
@@ -1,10 +1,11 @@
 import React, {useState} from "react";
-import { Link, Route, Routes, useNavigate } from "react-router-dom";
+import { Route, Routes, useNavigate } from "react-router-dom";
 import './App.css';
 // import components
 import Home from "./components/Home";
 import Camera from "./components/Camera";
 import Result from "./components/Result";
+import { ReactComponent as Icon } from "./components/accessibility-human.svg";
 
 const App = () => {
   const [image, setImage] = useState();
@@ -18,16 +19,19 @@ const App = () => {
   };
 
   return (
-    <div>
-      <div className="header">
-        <h1>姿勢改善アプリ</h1>
+    <div className="App">
+      <div className="App-header">
+        <Icon className="App-header-icon" />
+        <h1 className="App-header-name">姿勢改善アプリ</h1>
       </div>
 
-      <Routes>
-        <Route path="/" element={<Home navigate={navigate} />} />
-        <Route path="/camera" element={<Camera navigate={navigate} setMessage={setMessage} setImage={setImage} />} />
-        <Route path="/result" element={<Result message={message} image={image} reset={reset} />} />
-      </Routes>
+      <div className="App-main">
+        <Routes>
+          <Route path="/" element={<Home navigate={navigate} />} />
+          <Route path="/camera" element={<Camera navigate={navigate} setMessage={setMessage} setImage={setImage} />} />
+          <Route path="/result" element={<Result message={message} image={image} reset={reset} />} />
+        </Routes>
+      </div>
     </div>
   );
 };
--- front/src/components/Home.js
+++ front/src/components/Home.js
@@ -3,8 +3,13 @@ import React from "react";
 const Home = (props) => {
   return (
     <div>
-      <h2>Home</h2>
-      <p>AI姿勢推定で立ち姿勢を改善しよう！</p>
+      <div className="Home-headline">
+        <p className="Home-headline-p">AI姿勢推定で<br />立ち姿勢を改善しよう！</p>
+      </div>
+
+      <div className="Home-instruction">
+        <p className="Home-instruction-p">本アプリでは...(アプリの説明)</p>
+      </div>
 
       <div>
         <button onClick={() => props.navigate("/camera")}>はじめる</button>
