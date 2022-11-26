aa1c5f901fddf08d0d0ee23ce3b47009b908528c
Nysskyo <58655088+Nysskyo@users.noreply.github.com>
2022-11-26 20:13:48 +0900

    結果画面の見た目を修正

--- front/src/components/Result.js
+++ front/src/components/Result.js
@@ -10,51 +10,61 @@ const Result = (props) => {
 
       <div className="Result-container">
         <div className="Result-image-container">
-          <div className="Result-image-content">
-            <h3>正面</h3>
-            <div>
-              <img className="Result-image" src={`data:image/png;base64,${props.frontImage}`} alt="front" />
+          <div>
+            <div className="Result-image-res-container">
+              <div className="Result-image-res-content">
+                <h3>正面</h3>
+                <img className="Result-image-res" src={`data:image/png;base64,${props.frontImage}`} alt="front" />
+              </div>
+              
+              <div className="Result-image-res-content">
+                <h3>側面</h3>
+                <img className="Result-image-res" src={`data:image/png;base64,${props.sideImage}`} alt="side" />
+              </div>
             </div>
-            <div>
-              <img className="Result-image-example" src={exampleFront} alt="example front" />
-            </div>
-          </div>
 
-          <div className="Result-image-content">
-            <h3>側面</h3>
             <div>
-              <img className="Result-image" src={`data:image/png;base64,${props.sideImage}`} alt="side" />
+              <h3>お手本</h3>
             </div>
-            <div>
-              <img className="Result-image-example" src={exampleSide} alt="example side" />
+
+            <div className="Result-image-example-container">
+              <div className="Result-image-example-content">
+                <img className="Result-image-example" src={exampleFront} alt="example front" />
+              </div>
+
+              <div className="Result-image-example-content">
+                <img className="Result-image-example" src={exampleSide} alt="example side" />
+              </div>
             </div>
           </div>
         </div>
 
         <div className="Result-comment-container">
-          <div className="Result-comment-front">
-            <p>正面コメント</p>
-            <ul className="Result-comment-ul">
-              {props.frontMessages.map((message) => {
-                return (
-                  <li className="Result-comment-li">{message}</li>
-                )
-              })}
-            </ul>
-          </div>
-          <div className="Result-comment-side">
-            <p>側面コメント</p>
-            <ul className="Result-comment-ul">
-              {props.sideMessages.map((message) => {
-                return (
-                    <li className="Result-comment-li">{message}</li>
-                )
-              })}
-            </ul>
-          </div>
-
           <div>
-            <button className="Result-reset-button" onClick={props.reset}>もう一回</button>
+            <div className="Result-comment-front">
+              <p>正面コメント</p>
+              <ul className="Result-comment-ul">
+                {props.frontMessages.map((message) => {
+                  return (
+                    <li className="Result-comment-li">{message}</li>
+                  )
+                })}
+              </ul>
+            </div>
+            <div className="Result-comment-side">
+              <p>側面コメント</p>
+              <ul className="Result-comment-ul">
+                {props.sideMessages.map((message) => {
+                  return (
+                      <li className="Result-comment-li">{message}</li>
+                  )
+                })}
+              </ul>
+            </div>
+
+            <div>
+              <button className="Result-reset-button" onClick={props.reset}>もう一回</button>
+            </div>
           </div>
         </div>
       </div>
